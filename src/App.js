3d450aae09edd9f7bbd9f6d09f3501886ff5a2cf
Uvoi <iovu_dmtr@yahoo.com>
2023-10-26 17:23:10 +0300

    то же самое, но теперь работает

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './styles/bootstrap.min.css'
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import SloganCarousel from './components/SloganCarousel/SloganCarousel';
 import Content from './components/Content/Content';
-import Modal from './components/Modal/Modal';
 
 
 function App() {
