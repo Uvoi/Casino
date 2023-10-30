e4d344e54cf2c4ca15a616155920458fc85e36e7
Uvoi <iovu_dmtr@yahoo.com>
2023-10-30 12:36:51 +0300

    все работает!

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,8 +4,21 @@ import axios from 'axios';
 const Home = (props)=>
 {
 
+  axios.defaults.withCredentials = true;
   const check = () => {
-    axios.get('http://127.0.0.1:8000/api/whoami', { withCredentials: true })
+    axios.get('http://127.0.0.1:8000/api/whoami')
+      .then(response => {
+        console.log(response.data);
+        // использование данных сессии
+      })
+      .catch(error => {
+        console.log("ошибка блять!");
+        console.log(error);
+      });
+  }
+
+  const del = () => {
+    axios.post('http://127.0.0.1:8000/api/delete_session')
       .then(response => {
         console.log(response.data);
         // использование данных сессии
@@ -21,6 +34,7 @@ const Home = (props)=>
     <div>
       <SloganCarousel/>
       <button onClick={check} type='button'>check</button>
+      <button onClick={del}>delete</button>
     </div>
   );
 };
